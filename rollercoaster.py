976bdce1c5fedb540d229ff721a480c557436922
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2025-08-11 23:36:24 +0530

    Add files via upload

---
+++ rollercoaster.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster joyride!")
+height=int (input("What is your height in cm?"))
+bill=0
+if height>=120:
+    print("You can ride the rollercoaster!")
+    age=int(input("What is your age?"))
