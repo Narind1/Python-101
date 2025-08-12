c5a816800a6bfc742fc1f30197e167094555646b
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2025-08-13 00:40:18 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,16 @@
+'''for x in range(1,21):
+    if (x % 2 !=0):
+        print(x)'''
+'''list1=eval(input("Enter the list :"))
+s=0
+for x in list1:
+    s = s + x
+print(s)'''
+n=int(input("Enter value of n:"))
+i=1
+sum=0
+while ( i < n+1):
+    print(i)
+    sum=sum+i
+    i=i+1
+print("sum:",sum)
