c6bf0252578a5136e201046bda29bb71e95a3c6b
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2025-09-21 23:45:48 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,11 @@
+#reverse a string
+'''a=input("Enter a string")
+b=a[::-1]
+word= a.split()
+maxx= max(word,key=len)
+print(maxx)    
+a='my name is narind'
+print(a.count('i'))'''
+a=input("Enter")
+if a == a[::-1]:
+    print("Palindrome")
