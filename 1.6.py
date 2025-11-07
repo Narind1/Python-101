bdb81a3015a106a91a67360194b35a8187273878
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2025-11-07 23:54:01 +0530

    Add files via upload

---
+++ 1.6.py
@@ -0,0 +1,7 @@
+NAME=("NARIND VERMA")
+SAP_ID=500121898
+DATE_OF_BIRTH=("6TH MAY 2004")
+ADDRESS=("UPES \n      Bidholi campus \n      Pincode:248007")
+Programme=('AI & ML')
+SEMESTER=2
+print('NAME : ',NAME,"\nSAP ID :",SAP_ID,"\nDATE OF BIRTH :",DATE_OF_BIRTH,"\nADDRESS :",ADDRESS,"\nProgramme :",Programme,"\nSemester :",SEMESTER)
