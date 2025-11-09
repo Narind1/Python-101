684fd399d83d35a2835a1e7ca0b04b632bb2e1d0
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2025-11-09 23:59:36 +0530

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,12 @@
+x=9
+y=7
+z=x+y
+print("Addition :",z)
+z=x-y
+print("\nSubtraction :",z)
+z=x*y
+print("\nMultiplication :",z)
+z=x/y
+print("\nDivision :",z)
+z=x%y
+print("\nFloor division :",z)
