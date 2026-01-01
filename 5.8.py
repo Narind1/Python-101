a1444b10dc3e55b6ecc98af30db6f461ef1fabf7
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2026-01-01 23:45:38 +0530

    Add files via upload

---
+++ 5.8.py
@@ -0,0 +1,12 @@
+S1 = {"Red", "Yellow", "Orange", "Blue"}
+S2 = {"Violet", "Blue", "Purple"}
+print("S1:", S1)
+print("S2:", S2)
+union_result = S1.union(S2)
+print("Union of S1 and S2:", union_result)
+intersection_result = S1.intersection(S2)
+print("Intersection of S1 and S2:", intersection_result)
+difference_result = S1.difference(S2)
+print("Difference of S1 and S2:", difference_result)
+symmetric_difference_result = S1.symmetric_difference(S2)
+print("Symmetric Difference of S1 and S2:", symmetric_difference_result)
