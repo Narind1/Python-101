553f4d777446c1a05b8a57c87567ea3d290c8624
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2026-01-15 22:07:55 +0530

    Add files via upload

---
+++ 7.1.py
@@ -0,0 +1,10 @@
+#Write a Python function to find the maximum and minimum numbers from a sequence of numbers. (Note: Do not use built-in functions.)def 
+def min_max(f):
+    mn=[]
+    mx=[]
+    mx.append(max(f))
+    mn.append(min(f))
+    print(f"Minimum number:{mn}\nMaximum number:{mx}")
+a=[]
+a=input("Enter a sequence of numbers:")
+(min_max(a))
