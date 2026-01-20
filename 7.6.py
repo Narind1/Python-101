012fecd62dfbdb025787263d70dcb81abf10355a
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2026-01-20 23:46:05 +0530

    Add files via upload

---
+++ 7.6.py
@@ -0,0 +1,5 @@
+find_max_min = lambda lst: (max(lst), min(lst))
+input_list = [10, 6, 8, 90, 12, 56]
+result = find_max_min(input_list)
+print("Sample input:", input_list)
+print("Sample output:", result)
