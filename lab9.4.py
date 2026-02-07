483b2c07c92abb2c987b9df6c0f834009f0146c3
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2026-02-07 22:07:32 +0530

    Add files via upload

---
+++ lab9.4.py
@@ -0,0 +1,16 @@
+class Counter:
+    def __init__(self):
+        self.value = 0
+    def increment(self):
+        self.value += 1
+class CustomCounter(Counter):
+    def __init__(self,size):
+        Counter.__init__(self)
+        self.stepsize = size
+    def increment(self):
+        self.value += self.stepsize
+    def print_current(self):
+        print("Current value is ",self.value)
+cc = CustomCounter(4)
+cc.increment()
+cc.print_current()
