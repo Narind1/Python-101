8452ec55c755c899da2e3aef416105f38474f4f8
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2026-03-14 23:35:20 +0530

    Add files via upload

---
+++ doubt.py
@@ -0,0 +1,15 @@
+#1
+d={'roll_no':'16/001','name':'arav','corse':'btech'}
+#print("dict[rollnumber]",d['roll_no'],"dict[name]",d['name'])
+#print("dict[course]=",d['corse'])
+#d['marks']=95
+#print("dict[marks]=",d['marks'])
+#print(d)
+#print('marks is:',d.pop('marks',0))
+print("after randomly popping the dict is:",d.popitem())
+print(d)
+#2
+#set is ordered or unordered.
+s=set([4,3,2,1])
+print(s)
+print(set(1,2,4,3))
